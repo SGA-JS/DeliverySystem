ba5a292812837ed82c7ec120eb691fa31ca584b1
GASAW <saw.gweaw@gmail.com>
2024-03-03 04:12:50 +0800

    add Login, Signup,Map activity layout

--- app/src/main/java/com/example/deliverysystem/LoginActivity.java
+++ app/src/main/java/com/example/deliverysystem/LoginActivity.java
@@ -1,14 +1,28 @@
 package com.example.deliverysystem;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login); // Make sure this layout file has a Button with the ID login_button
+
+        Button loginButton = findViewById(R.id.buttonLogin); // Replace with the actual ID of your login button
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent to start MainActivity
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                // Optionally, if you want to finish LoginActivity so it's removed from the back stack
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/deliverysystem/SignupActivity.java
+++ app/src/main/java/com/example/deliverysystem/SignupActivity.java
@@ -2,7 +2,9 @@ package com.example.deliverysystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void buttonSignupClicked(View view) {
+        Intent myIntent = new Intent(SignupActivity.this, LoginActivity.class);
+        // Switch to the AnotherActivity
+        startActivity(myIntent);
+        finish();
     }
 }
