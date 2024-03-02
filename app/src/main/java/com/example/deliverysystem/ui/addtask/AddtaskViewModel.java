1877037399eeca1f5d89f85765a408e0685ae8ab
GASAW <saw.gweaw@gmail.com>
2024-03-03 04:15:10 +0800

    add list and task fragment following the exiting template.

---
+++ app/src/main/java/com/example/deliverysystem/ui/addtask/AddtaskViewModel.java
@@ -0,0 +1,19 @@
+package com.example.deliverysystem.ui.addtask;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class AddtaskViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public AddtaskViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is Addtask fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
