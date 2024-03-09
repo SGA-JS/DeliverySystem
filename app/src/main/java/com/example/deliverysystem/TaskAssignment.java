81ddba2a2829b11b90445c34b9e9ac8fea220d51
GASAW <saw.gweaw@gmail.com>
2024-03-09 14:58:26 +0800

    task assignment

---
+++ app/src/main/java/com/example/deliverysystem/TaskAssignment.java
@@ -0,0 +1,14 @@
+package com.example.deliverysystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaskAssignment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_assignment);
+    }
+}
